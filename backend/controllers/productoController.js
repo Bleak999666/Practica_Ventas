e40df64e5b0d42bdf45d59b49a0962239d0c4f83
SETROPGOD12 <andresfeliportez@gmail.com>
2025-05-22 11:10:52 -0500

    ventas_inicio

---
+++ backend/controllers/productoController.js
@@ -0,0 +1,15 @@
+import { addProducto, getProductos} from '../models/productoModels.js';
+
+export const obtenerProductos = (req, res) => (
+    getProductos((err, resultados_prod) => {
+        if (err) return res.status(500).json({error: err.message});
+        res.json(resultados_prod);
+    })
+);
+export const crearProducto = (req, res) => {
+    const producto = req.body;
+    addProducto(producto, (err,resultado_prod) =>{
+        if (err) return res.status(500).json({error: err.message});
+        res.json ({mensaje_prod: 'Producto agregado', id: resultado_prod.insertId});
+    });
+};
